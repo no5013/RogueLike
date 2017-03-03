942ff27eb8602d34267a2163f234f96481a428c9
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-03-03 10:49:07 +0700

    Import Project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Player : MovingObject {
+
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+
+	// Use this for initialization
+	protected override void Start () {
+        animator = GetComponent<Animator>();
+
+        food = GameManager.instance.playerFoodPoints;
+
+        base.Start();
+	}
+	
+    private void OnDisable()
+    {
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+	// Update is called once per frame
+	void Update () {
+//If it's not the player's turn, exit the function.
+            if(!GameManager.instance.playersTurn) return;
+            
+            int horizontal = 0;     //Used to store the horizontal move direction.
+            int vertical = 0;       //Used to store the vertical move direction.
+            
+            
+            //Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+            horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
+            
+            //Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+            vertical = (int) (Input.GetAxisRaw ("Vertical"));
+            
+            //Check if moving horizontally, if so set vertical to zero.
+            if(horizontal != 0)
+            {
+                vertical = 0;
+            }
+            
+            //Check if we have a non-zero value for horizontal or vertical
+            if(horizontal != 0 || vertical != 0)
+            {
+                //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+                //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+                AttempMove<Wall> (horizontal, vertical);
+            }
+
+    }
+
+    protected override void AttempMove<T>(int xDir, int yDir)
+    {
+        food--;
+
+        base.AttempMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+
+        CheckIfGameOver();
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            //Invoke the Restart function to start the next level with a delay of restartLevelDelay (default 1 second).
+            Invoke("Restart", restartLevelDelay);
+
+            //Disable the player object since level is over.
+            enabled = false;
+        }
+
+        //Check if the tag of the trigger collided with is Food.
+        else if (other.tag == "Food")
+        {
+            //Add pointsPerFood to the players current food total.
+            food += pointsPerFood;
+
+            //Disable the food object the player collided with.
+            other.gameObject.SetActive(false);
+        }
+
+        //Check if the tag of the trigger collided with is Soda.
+        else if (other.tag == "Soda")
+        {
+            //Add pointsPerSoda to players food points total
+            food += pointsPerSoda;
+
+
+            //Disable the soda object the player collided with.
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+       
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+            GameManager.instance.GameOver();
+    }
+
+    private void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        CheckIfGameOver();
+    }
+}
