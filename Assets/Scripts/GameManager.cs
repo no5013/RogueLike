23d75f82ab0e1a1ece6f5d25a1e7e4bd9c738556
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-03-04 03:11:08 +0700

    Add LevelImage when start the level and end of the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+	public float levelStartDelay = 2f;
     public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
@@ -11,9 +13,12 @@ public class GameManager : MonoBehaviour {
     [HideInInspector]
     public bool playersTurn = true;
 
+	private Text levelText;
+	private GameObject levelImage;
     private int level = 3;
     private List<Enemy> enemies;
     private bool enemiesMoving;
+	private bool doingSetup;
 
 	// Use this for initialization
 	void Awake () {
@@ -31,17 +36,35 @@ public class GameManager : MonoBehaviour {
 
 	}
 
+	private void OnlevelWasLoaded(int index){
+		level++;
+		InitGame();
+	}
+
     public void GameOver()
     {
+		levelText.text = "After " + level + " days, you starved.";
+		levelImage.SetActive (true);
         enabled = false;
     }
 
     void InitGame()
     {
+		doingSetup = true;
+		levelImage = GameObject.Find ("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text> ();
+		levelText.text = "Day " + level;
+		Invoke ("HideLevelImage", levelStartDelay);
+
         enemies.Clear();
         boardScript.SetupScene(level);
     }
-	
+
+	private void HideLevelImage(){
+		levelImage.SetActive (false);
+		doingSetup = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
         if (playersTurn || enemiesMoving)
