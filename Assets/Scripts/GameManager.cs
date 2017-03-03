18fde90b9fe1b5197a1197056f2bbd68c318f4b1
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-03-04 03:33:52 +0700

    Fix the bug that can't end the level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour {
 
 	private Text levelText;
 	private GameObject levelImage;
-    private int level = 3;
+    private int level = 1;
     private List<Enemy> enemies;
     private bool enemiesMoving;
 	private bool doingSetup;
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour {
 
 	}
 
-	private void OnlevelWasLoaded(int index){
+	private void OnLevelWasLoaded(int index){
 		level++;
 		InitGame();
 	}
